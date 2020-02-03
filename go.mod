73d8e75da08e008d60d42c48587bbc6fc38c128d
Michael Hausenblas <hausenbl@amazon.com>
2020-02-03 09:47:54 +0000

    improves rendering for single layer
    Signed-off-by: Michael Hausenblas <hausenbl@amazon.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/mhausenblas/l2i
 
 go 1.13
 
-require github.com/aws/aws-sdk-go v1.28.9 // indirect
+require (
+	github.com/aws/aws-sdk-go v1.28.9
+	golang.org/x/text v0.3.2
+)
--- main.go
+++ main.go
@@ -1,39 +1,70 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/arn"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/lambda"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 func main() {
-	if len(os.Args[1:]) < 1 {
-		log.Fatalln("Need the ARN of the Lambda layer sorry :(")
+	numargs := len(os.Args[1:])
+	if numargs < 1 {
+		log.Fatalln("Need at least one ARN of a Lambda layer, sorry :(")
 	}
-	// the ARN of the Lambda layer has to be the first argument:
-	layerarn := os.Args[1]
-	// look up metadata and content:
-	linfo, err := resolve(layerarn)
-	if err != nil {
-		log.Fatalf("Can't diagnose Lambda layer based on the ARN %s", layerarn)
+	switch numargs {
+	case 1:
+		// the ARN of the Lambda layer has to be the first argument:
+		larns := os.Args[1]
+		// look up metadata and content:
+		linfo, larn, err := resolve(larns)
+		if err != nil {
+			log.Fatalf("Can't diagnose Lambda layer based on the ARN %s: %v", larns, err)
+		}
+		err = render(larn, linfo)
+		if err != nil {
+			log.Fatalf("Can't resolve Lambda layer location: %v", err)
+		}
+	default:
+		log.Println("render all layers")
 	}
-	log.Printf("%v", linfo)
 }
 
 // resolve looks up metadata and content of a AWS Lambda layer by ARN
-func resolve(layerarn string) (*lambda.GetLayerVersionByArnOutput, error) {
-	layer, err := arn.Parse(layerarn)
+func resolve(larns string) (*lambda.GetLayerVersionByArnOutput, arn.ARN, error) {
+	larn, err := arn.Parse(larns)
 	if err != nil {
-		return nil, err
+		return nil, larn, err
 	}
-	svc := lambda.New(session.Must(session.NewSession()), aws.NewConfig().WithRegion(layer.Region))
+	svc := lambda.New(session.Must(session.NewSession()), aws.NewConfig().WithRegion(larn.Region))
 	vo, err := svc.GetLayerVersionByArn(
 		&lambda.GetLayerVersionByArnInput{
-			Arn: aws.String(layer.String()),
+			Arn: aws.String(larn.String()),
 		})
-	return vo, nil
+	return vo, larn, nil
+}
+
+// render displays info about a single Lambda layer
+func render(larn arn.ARN, linfo *lambda.GetLayerVersionByArnOutput) error {
+	fmt.Printf("Name: %v\n", strings.Split(larn.Resource, ":")[1])
+	fmt.Printf("Version: %v\n", *linfo.Version)
+	fmt.Printf("Description: %v\n", *linfo.Description)
+	fmt.Printf("Created on: %v\n", *linfo.CreatedDate)
+	p := message.NewPrinter(language.English)
+	p.Printf("Size: %v kB\n", *linfo.Content.CodeSize/1024)
+	lloc, err := url.Parse(*linfo.Content.Location)
+	if err != nil {
+		return err
+	}
+	q := lloc.Query()
+	fmt.Printf("Location: %v://%v%v?versionId=%v\n", lloc.Scheme, lloc.Host, lloc.Path, q.Get("versionId"))
+	return nil
 }
